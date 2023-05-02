91d47598b3232fc2d2ab6f0b5ee2af303b4e0d69
IT21202254 <IT21202254@my.sliit.lk>
2023-05-02 19:00:36 +0530

    create 3rd page

--- app/src/main/java/com/example/quickloan/Ks_MainActivity.kt
+++ app/src/main/java/com/example/quickloan/Ks_MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.quickloan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class KsMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ks_applyloan_a)
+        setContentView(R.layout.ks_applyloan_b)
+
+        val firebase : DatabaseReference = FirebaseDatabase.getInstance().getReference()
     }
 }
