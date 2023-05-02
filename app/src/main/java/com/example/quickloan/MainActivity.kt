91fff4f4d596b82547a2c1b63cdc081521b120d5
IT21306518 <99585037+IT21306518@users.noreply.github.com>
2023-05-02 18:01:16 +0530

    create Dashboard ui

--- app/src/main/java/com/example/quickloan/MainActivity.kt
+++ app/src/main/java/com/example/quickloan/MainActivity.kt
@@ -1,18 +1,46 @@
 package com.example.quickloan
 
-import android.os.Bundle
-import android.view.Menu
-import android.widget.TextView
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
-
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var loanAmount: EditText
+    private lateinit var annualInterestRate: EditText
+    private lateinit var loanPeriod: EditText
+    private lateinit var calculateButton: Button
+    private lateinit var resultTextView: TextView
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.bu_calculator)
+
+        loanAmount = findViewById(R.id.loan_amount_edit_text)
+        annualInterestRate = findViewById(R.id.annual_interest_rate_edit_text)
+        loanPeriod = findViewById(R.id.loan_period_edit_text)
+        calculateButton = findViewById(R.id.calculate_button)
+        resultTextView = findViewById(R.id.result_text_view)
+
+        calculateButton.setOnClickListener { calculateLoanPayment() }
+    }
+
+    @SuppressLint("StringFormatInvalid")
+    private fun calculateLoanPayment() {
+        val principal = loanAmount.text.toString().toDouble()
+        val annualRate = annualInterestRate.text.toString().toDouble()
+        val loanYears = loanPeriod.text.toString().toDouble()
+
+        val loanMonths = loanYears * 12
+        val monthlyRate = annualRate / 12 / 100
+        val monthlyPayment = principal * monthlyRate / (1 - (1 / Math.pow(1 + monthlyRate, loanMonths)))
+
+        val formattedPayment = NumberFormat.getCurrencyInstance().format(monthlyPayment)
+
+        resultTextView.text = getString(R.string.monthly_payment_result, formattedPayment)
     }
 }
-
