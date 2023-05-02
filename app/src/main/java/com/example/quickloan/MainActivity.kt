776fceafd40521b872c7116bb56a79f6921f8fec
IT21306518 <99585037+IT21306518@users.noreply.github.com>
2023-05-02 13:07:06 +0530

    Create Dashboard

--- app/src/main/java/com/example/quickloan/MainActivity.kt
+++ app/src/main/java/com/example/quickloan/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.quickloan
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
+
