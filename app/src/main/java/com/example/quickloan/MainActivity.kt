40ab924afef1309d619606761e017cf845734fc2
IT21202254 <IT21202254@my.sliit.lk>
2023-05-02 20:30:32 +0530

    connect db

--- app/src/main/java/com/example/quickloan/MainActivity.kt
+++ app/src/main/java/com/example/quickloan/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
-class KsMainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ks_applyloan_a)
