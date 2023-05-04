0b6461680c226c3bd17a0f3e6d394253ad0d7e60
IT21306518 <99585037+IT21306518@users.noreply.github.com>
2023-05-04 14:24:52 +0530

    add pages

--- app/src/main/java/com/example/quickloan/MainActivity.kt
+++ app/src/main/java/com/example/quickloan/MainActivity.kt
@@ -1,19 +1,29 @@
 package com.example.quickloan
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+//    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
- 
+        setContentView(R.layout.ks_applyloan_a);
 
-        setContentView(R.layout.bu_dashboard)
- 
-        setContentView(R.layout.ks_applyloan_b)
- 
+//        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+//
+//        findViewById<View>(R.id.imageMenu).setOnClickListener {
+//            drawerLayout.openDrawer(GravityCompat.START)
+//        }
+
+
+
+
 
     }
 }
