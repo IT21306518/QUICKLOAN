6c493d8218c3a7202b94652a505e72b959a97694
IT21202254 <IT21202254@my.sliit.lk>
2023-05-04 17:31:38 +0530

    insert deta

---
+++ app/src/main/java/com/example/quickloan/KsLoneModel.kt
@@ -0,0 +1,12 @@
+package com.example.quickloan
+
+data class KsLoneModel (
+   var loneId: String? = null,
+   var fName: String? = null,
+   var mName: String? = null,
+   var lName: String? = null,
+   var nic: String? = null,
+   var bDate: String? = null,
+   var emails: String? = null,
+
+)
--- app/src/main/java/com/example/quickloan/MainActivity.kt
+++ app/src/main/java/com/example/quickloan/MainActivity.kt
@@ -2,18 +2,21 @@ package com.example.quickloan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
- 
+//        setContentView(R.layout.bu_dashboard)
 
-        setContentView(R.layout.bu_dashboard)
- 
-        setContentView(R.layout.ks_applyloan_d)
- 
+
+
+
+
 
     }
 }
